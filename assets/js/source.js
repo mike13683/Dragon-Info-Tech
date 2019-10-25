446b3f4e1e87a6d5710f9163b4e786d7d67b31f2
unknown <mike@dragoninfotech.com>
2019-10-25 20:48:10 +0100

    remade our work

--- assets/js/source.js
+++ assets/js/source.js
@@ -112,91 +112,80 @@ $(document).ready(function(){
         }) // this requires a plugin
         .addTo(controller);
 
-    // homepage - intro
-    // var parallaxTl = new TimelineLite();
-    // parallaxTl
-    // // .to('.intro__content', 0.3, {autoAlpha: 0})
-    // // .fromTo('.js-hook-intro-1', 1, {backgroundColor:"green", scaleY:1, height:"100vh"}, { scaleY:0, height:"0"})
-    //     .fromTo('.js-hook-intro-1', 2, {backgroundColor:"#343A40"}, {backgroundColor:"#ED1C24"})
-    //     .fromTo('.js-hook-intro-2', 2, {backgroundColor:"#ED1C24"}, {backgroundColor:"#343A40"}, "-=1.5")
-    // ;
-    //
-    // var slideParallaxScene = new ScrollMagic.Scene({
-    //     triggerElement: '#intro',
-    //     triggerHook: 0,
-    //     offset: -35,
-    //     reverse:false
-    // })
-    //     .setTween(parallaxTl)
-    //     .addIndicators({
-    //         name: 'intro',
-    //         colorTrigger: 'orange',
-    //         colorStart: '#75C695',
-    //         colorEnd: 'pink'
-    //     })
-    //
-    //     .addTo(controller);
+// homepage - intro
+    (function($) {
 
-    // // homepage - intro
-    // var controller = new ScrollMagic.Controller();
-    //
-    // // pin the intro
-    // var pinIntroScene = new ScrollMagic.Scene({
-    //     triggerElement: '.intro__header',
-    //     triggerHook: 0,
-    //     duration: '5%'
-    // })
-    //     .setPin('.intro__content', {pushFollowers: false})
-    //     .addTo(controller);
+        var introBgPrimary = $(".introBgPrimary"),
+            introBgSecondary = $(".introBgSecondary"),
+            introContent = $(".intro__content h1 p"),
+            intro__bodyH2 = $(".intro__body h2"),
+            intro__bodyH2Mobile = $(".intro__body--mobile-hook h2"),
+            intro__bodySpan = $(".intro__body span")
+        ;
+
+        var tl = new TimelineLite();
+        tl.to(introBgPrimary, 0.6, {scaleY:1, backgroundColor:"#ED1C24"})
+        tl.to(introBgSecondary, 0.6, {scaleY:1, backgroundColor:"#343A40"}, "-=0.6")
+        tl.staggerFrom(introContent, 0.6, {autoAlpha:0, x:200}, 0.3)
+        tl.staggerFrom(intro__bodyH2, 0.5, {text:""},2, "-=2.0")
+        tl.staggerFrom(intro__bodySpan, 0.6, {text:""},2,"+=0.3")
+        ;
+
+        // Mobile h2 heading
+        var tl2 = new TimelineLite();
+        tl2.fromTo(intro__bodyH2Mobile, 0.5, {y:20,autoAlpha:0}, {y:0, autoAlpha:1},2);
+
+        // Our work
+
+        var controller = new ScrollMagic.Controller();
+
+        $('.work__content').each(function(){
+            var ourWorkTl = new TimelineLite();
+            ourWorkTl.fromTo(this, 11, {autoAlpha:1}, {autoAlpha:0})
+            // ourWorkTl.fromTo(".work__media img", 1, {x:100}, {x:0}, "-=1")
+            ;
+
+            var ourWorkScene = new ScrollMagic.Scene({
+                triggerElement: this,
+                triggerHook: 0.3,
+                duration: "95%"
+            })
+
+                .setPin(this)
+                .setTween(ourWorkTl)
+                .addIndicators({
+                    name: 'our work pin 1',
+                    colorTrigger: 'black',
+                    colorStart: '#75C695',
+                    colorEnd: 'pink'
+                })
+
+                .addTo(controller);
+        });
+
+    })(jQuery);
+
+    (function($) {
+        var controller = new ScrollMagic.Controller();
+
+        var tlOurWork = new TimelineLite();
+        tlOurWork.fromTo(".our-work-contact-js-hook", 2.5, {scaleX:0, transformOrigin:"left", backgroundColor:"#343a40"},{ backgroundColor:"#2C3240", scaleX:1});
+
+        var ourWorkContactScene = new ScrollMagic.Scene({
+            triggerElement: ".our-work-contact-js-hook",
+            triggerHook: 0.8
+        })
+
+            .setTween(tlOurWork)
+            .addIndicators({
+                name: 'OurWorkContactScene',
+                colorTrigger: 'black',
+                colorStart: '#75C695',
+                colorEnd: 'pink'
+            })
+            .addTo(controller);
+
+
+    })(jQuery);
 
 });
-
-// homepage - intro
-(function($) {
-
-//     // jQuery Selectors
-// //     var intro = $("#intro"),
-// //         intro__header = $(".intro__header"),
-// //         intro__body = $(".intro__body");
-// //
-// //         // Simple Tween
-// //     TweenLite.fromTo(intro, 2.5, {autoAlpha: 1, x:100}, { autoAlpha: 1, x:0, ease: Elastic. easeOut.config( 1, 0.4)})
-// //     TweenLite.fromTo(intro__header, 2.5, { autoAlpha: 0, y:50}, { autoAlpha: 1, y:0, delay:1.0})
-// //
-// // })(jQuery);
-// //
-// // (function($) {
-// //
-// // var tl = new TimelineLite();
-// //     tl.staggerFrom(".intro__header h1 p", 1.0, {y:50, opacity: 0}, 1.5)
-// //     tl.staggerTo(".intro__header h1 p", 1.0, {y:0, opacity: 1}, 1.5, 0.5);
-// //     tl.fromTo("#intro h2", 1.0, { autoAlpha: 0, y:-50}, { autoAlpha: 1, y:0}, "-=0.8")
-// //     tl.fromTo("#intro span", 1.0, { autoAlpha: 0, y:-50}, { autoAlpha: 1, y:50}, "-=0.9")
-// //    ;
-
-
-    var introBgPrimary = $(".introBgPrimary"),
-        introBgSecondary = $(".introBgSecondary"),
-        introContent = $(".intro__content h1 p"),
-        intro__bodyH2 = $(".intro__body h2"),
-        intro__bodyH2Mobile = $(".intro__body--mobile-hook h2"),
-        intro__bodySpan = $(".intro__body span")
-    ;
-
-    var tl = new TimelineLite();
-    tl.to(introBgPrimary, 0.6, {scaleY:1, backgroundColor:"#ED1C24"})
-    tl.to(introBgSecondary, 0.6, {scaleY:1, backgroundColor:"#343A40"}, "-=0.6")
-    tl.staggerFrom(introContent, 0.6, {autoAlpha:0, x:200}, 0.3)
-    tl.staggerFrom(intro__bodyH2, 0.5, {text:""},2, "-=2.0")
-    tl.staggerFrom(intro__bodySpan, 0.6, {text:""},2,"+=0.3")
-    ;
-
-    // Mobile h2 heading
-    var tl2 = new TimelineLite();
-    tl2.fromTo(intro__bodyH2Mobile, 0.5, {y:20,autoAlpha:0}, {y:0, autoAlpha:1},2);
-
-})(jQuery);
-
-
-
-
